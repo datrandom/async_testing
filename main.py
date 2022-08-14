7c9a5b81dd3f7cc564c511ce5523a2b44d0c8dcf
Qfront <xeone5690@gmail.com>
2022-08-14 06:37:38 +0500

    created first async test

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#  test
+import asyncio
+import random
+
+
+async def main():
+    for i in range(100):
+        asyncio.create_task(load_data(i))
+    await asyncio.sleep(3)
+
+
+async def load_data(i: int):
+    sleep_time = random.uniform(0, 2.6)
+    await asyncio.sleep(sleep_time)
+    print(f"some data {i}, loading time: {sleep_time}")
+
+asyncio.run(main())
+
