dd69eb7f64ea2925adcc99e26f44449aa283ccaf
Qfront <xeone5690@gmail.com>
2022-08-15 06:01:51 +0500

    test with two tasks, one task - 2.7sec, two tasks - 1.3-1.5sec

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import asyncio
 import random
+from datetime import datetime
+
+
+last_response_time = datetime.now()
+response_list = []
+
+
+async def get_data(name: str, current_loop: int):
+    global last_response_time
+    sleep_time = round(random.uniform(1.9, 3.5), 2)
+    await asyncio.sleep(sleep_time)
+    print(datetime.now() - last_response_time)
+    response_list.append(datetime.now() - last_response_time)
+    last_response_time = datetime.now()
 
 
 async def main():
-    for i in range(100):
-        asyncio.create_task(load_data(i))
-    await asyncio.sleep(3)
+    a = 0
+    for _ in range(50):
+        a += 1
+        task1 = asyncio.create_task(get_data("Task1", a))
+        await asyncio.sleep(2)
+        task2 = asyncio.create_task(get_data("Task2", a))
+        await task1
 
+if __name__ == "__main__":
+    asyncio.run(main())
+    summ = datetime.now() - datetime.now()
+    for i in response_list:
+        summ += i
 
-async def load_data(i: int):
-    sleep_time = random.uniform(0, 2.6)
-    await asyncio.sleep(sleep_time)
-    print(f"some data {i}, loading time: {sleep_time}")
+    print(f"Average waiting for response: {summ/len(response_list)}")
 
-asyncio.run(main())
-
+    breakpoint()
