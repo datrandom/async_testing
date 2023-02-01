cd5186a0ee2ea483eb739bc42e8046c4eb4296e6
Qfront <xeone5690@gmail.com>
2023-02-01 20:58:14 +0500

    async_socket_1 - простейшая асинхронность через использование функции select()

--- async_socket_1.py
+++ async_socket_1.py
@@ -1,44 +1,57 @@
 import socket
-import selectors
+from select import select
 
 
+# Запуск Сервера -----------------------------------------------------
 # Starting IPv4 server (AF_INET -> IPv4, SOCK_STREAM -> TCP)
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Set server socket option: Reuse address = True
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Start on 127.0.0.1 : 5000
-server.bind(("localhost", 5000))
+server_socket.bind(("localhost", 5000))
 # Listening for incoming requests
-server.listen()
+server_socket.listen()
+
+# Список для мониторинга состояний -----------------------------------
+to_monitor = []
 
 
-# Принимает соединение: возвращает сокет клиента и ip:port
+# Принимает соединение и добавляет клиентский сокет в список
 def accept_connections(server_socket):
     client_socket, client_address = server_socket.accept()
-    return client_socket, client_address
+    print(f"New connection - {client_address}")
+
+    to_monitor.append(client_socket)
 
 
-# Цикл который ожидает запроса от клиента и отправляет ответ, в случае пустого запроса прерывает соединение
+# Принимает клиентский сокет => получает запрос => отправляет ответ / закрывает соединение, если запроса нету
 def live_connection(client_socket):
-    while True:
-        request = client_socket.recv(4096)
-        if request:
-            response = "Hello world!\n".encode()
-            client_socket.send(response)
-        else:
-            break
-    client_socket.close()
+    request = client_socket.recv(4096)
+    if request:
+        response = "Hello world!\n".encode()
+        client_socket.send(response)
+    else:
+        client_socket.close()
 
 
-# Цикл который ожидает подключений к серверу, в случае нового соединения запускает цикл приема запросов
-def run():
+# Вечный цикл в котором мониторятся состояния сокетов
+def event_loop():
     while True:
-        # Waiting for new connections
-        client_socket, client_address = accept_connections(server)
-        print(f"New connection - {client_address}")
-        # Waiting for requests from a client
-        live_connection(client_socket)
+        # Функция select возвращает сокеты у которых изменилось состояние и они готовы к чтению
+        ready_to_read, _, _ = select(to_monitor, [], [])  # read, write, errors
+
+        # Появляется сокет который готов к чтению
+        for sock in ready_to_read:
+            # Если изменилось состояние серверного сокета
+            if sock is server_socket:
+                # Принимает соединение и добавляет клиентский сокет к мониторингу
+                accept_connections(server_socket)
+            # Если изменилось состояние клиентского сокета
+            else:
+                # Принимает запрос от клиентского сокета
+                live_connection(sock)
 
 
 if __name__ == "__main__":
-    run()
+    to_monitor.append(server_socket)
+    event_loop()
